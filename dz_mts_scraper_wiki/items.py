a1393336f32a639c393654a08c96eb4a62570acc
LeonidChud <leonid.chudnovskii@gmail.com>
2026-01-26 12:58:34 +0300

    add all files project

---
+++ dz_mts_scraper_wiki/items.py
@@ -0,0 +1,14 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class FilmItem(scrapy.Item):
+    film_name = scrapy.Field()
+    genre = scrapy.Field()
+    director = scrapy.Field()
+    country = scrapy.Field()
+    year = scrapy.Field()
---
+++ dz_mts_scraper_wiki/spiders/wiki.py
@@ -0,0 +1,51 @@
+import scrapy
+from dz_mts_scraper_wiki.items import FilmItem
+
+class WikiSpider(scrapy.Spider):
+    name = "wiki"
+    allowed_domains = ["ru.wikipedia.org"]
+    start_urls = ["https://ru.wikipedia.org/wiki/Категория:Фильмы_по_алфавиту"]
+
+    def parse(self, response):
+        for selector in response.css('div.mw-category-group li'):
+            film_name = selector.css('a::attr(title)').get()
+            film_page = selector.css('a::attr(href)').get()
+            
+            if film_page:
+                yield response.follow(
+                    film_page,
+                    callback=self.parse_film_info,
+                    meta={'film_name': film_name}
+                )
+            else: 
+                yield film_name
+
+            
+
+        next_page_link = response.css('a:contains("Следующая страница")::attr(href)').get()
+        if next_page_link:
+            yield response.follow(next_page_link, callback=self.parse)
+
+    def parse_film_info(self, response):
+        item = FilmItem()
+
+        item['film_name'] = response.meta.get('film_name', 'Unknown')
+    
+        selector_genre = response.css('span[data-wikidata-property-id="P136"]')
+        item['genre'] = selector_genre.css('a::attr(title)').get()
+    
+        selector_director = response.css('span[data-wikidata-property-id="P57"]')
+        item['director'] = selector_director.css('a::attr(title)').get()
+    
+        selector_county = response.css('span[data-wikidata-property-id="P495"]')
+        item['country'] = selector_county.css('a::attr(title)').get()
+    
+    
+        selector_year = response.css('tbody tr:contains("Год")')
+        if selector_year:
+            item['year'] = selector_year.css('span.dtstart::text').get()
+        else:
+            selector_year = response.css('tbody tr:contains("Дата")')
+            item['year'] = selector_year.css('td.plainlist::text').get().replace("\n", "")
+    
+        yield item
